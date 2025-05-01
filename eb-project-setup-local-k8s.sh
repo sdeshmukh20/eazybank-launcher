86a1f2d242e5e93220cf7c5f05117eacfd622983
Satyajeet Deshmukh <sdeshmukh@mobiquityinc.com>
2025-05-01 22:48:12 +0530

    K8S support launcher script

--- eb-project-setup-local-k8s.sh
+++ eb-project-setup-local-k8s.sh
@@ -16,7 +16,7 @@ fi
 
 # Check if the environment name is provided
 if [ -z "$2" ]; then
-  print_message "Environment name is required (e.g.dev,uat). Exiting."
+  print_message "Deployment branch-name is required (e.g.release1.0). Exiting."
   exit 1
 fi
 
@@ -29,25 +29,31 @@ print_message "Setting up EB-System in directory: ${ROOT_DIRECTORY}"
 #SERVICE_DIR="${ROOT_DIRECTORY}/eazybank-system/services"
 cd "${ROOT_DIRECTORY}"
 print_message "Creating directories..."
-mkdir -p "eazybank-system/infra"
-mkdir -p "eazybank-system/services"
+mkdir -p "eazybank-system/infra/eazybank-deployment"
+mkdir -p "eazybank-system/services/eazybank-service"
+mkdir -p "eazybank-system/client-ui/eazy-bank-ui"
 
 # 2. Clone repositories
 print_message "Cloning eazybank-service repository."
 cd eazybank-system
-cp -r /Users/sdeshmukh/DEV/visionmax/syestem24/services/eazybank-service services
+SERVICE_BRANCH="$2"
+git clone -b "$SERVICE_BRANCH" https://github.com/sdeshmukh20/eazybank-service.git services/eazybank-service
 
 print_message "Cloning eazybank-deployment repository..."
+DEPLOYMENT_BRANCH="$2"
 #read -p "Enter the branch name for eazybank-deployment: " DEPLOYMENT_BRANCH
-cp -r /Users/sdeshmukh/DEV/visionmax/syestem24/infra/test-repo/eazybank-deployment infra
+git clone -b "$DEPLOYMENT_BRANCH" https://github.com/sdeshmukh20/eazybank-deployment.git infra/eazybank-deployment
+
+print_message "Cloning eazybank-ui repository..."
+
+#read -p "Enter the branch name for eazybank-client-ui: " DEPLOYMENT_BRANCH
+git clone -b "$DEPLOYMENT_BRANCH" https://github.com/sdeshmukh20/eazy-bank-ui client-ui/eazy-bank-ui
 
 
 # 5. Print success message
-print_message "Setup completed successfully!"
+print_message "Backend code downloaded successfully!"
 
-cd ../../infra/eazybank-deployment/bootstrap-k8s
-print_message "Executing bootstrap from launcher"
+cd ./infra/eazybank-deployment/bootstrap-k8s
+print_message "Now Execute bootstrap commands:"
 chmod +x ./main.sh
-
-cat README.md
-
+#./main.sh $2
